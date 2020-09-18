26589584a5f8fd7f565dd4607891cbe2bc2667e1
Prakanshu184 <68950307+Prakanshu184@users.noreply.github.com>
2020-09-18 16:09:26 +0530

    Add files via upload

---
+++ assgnment.js
@@ -0,0 +1,18 @@
+function image1() {
+    const img1 = document.getElementsByTagName("img");
+    let url =
+      "https://cdn.pixabay.com/photo/2015/09/05/20/02/coding-924920__340.jpg";
+    img1[0].src = url;
+  }
+  function image2() {
+    const img2 = document.getElementsByTagName("img");
+    let url =
+      "https://cdn.pixabay.com/photo/2017/01/22/12/07/imac-1999636__340.png";
+    img2[0].src = url;
+  }
+  function image3() {
+    const img3 = document.getElementsByTagName("img");
+    let url =
+      "https://cdn.pixabay.com/photo/2016/12/28/09/36/web-1935737__340.png";
+    img3[0].src = url;
+  }
